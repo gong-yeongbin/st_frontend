05d7f1a292ba7dd0d08501392669c5ff652864ec
yeongbin-gong <youngbin816@gmail.com>
2023-02-22 12:50:51 +0900

    prettier, eslint install

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-	import CheckedMoreThanFiveBillion from './CheckedMoreThanFiveBillion.svelte'
-	import MoreThan15percentComparedToThePreviousDay from './MoreThan15percentComparedToThePreviousDay.svelte'
-	import ANetPurchaseOfThePreviousDayMoreThan10BillionWon from './ANetPurchaseOfThePreviousDayMoreThan10BillionWon.svelte'
-	import TransactionAmountOfThePreviousDayMoreThan100BillionWon from './TransactionAmountOfThePreviousDayMoreThan100BillionWon.svelte'
-
+  import CheckedMoreThanFiveBillion from './CheckedMoreThanFiveBillion.svelte';
+  import MoreThan15percentComparedToThePreviousDay from './MoreThan15percentComparedToThePreviousDay.svelte';
+  import ANetPurchaseOfThePreviousDayMoreThan10BillionWon from './ANetPurchaseOfThePreviousDayMoreThan10BillionWon.svelte';
+  import TransactionAmountOfThePreviousDayMoreThan100BillionWon from './TransactionAmountOfThePreviousDayMoreThan100BillionWon.svelte';
 </script>
 
 <CheckedMoreThanFiveBillion />
