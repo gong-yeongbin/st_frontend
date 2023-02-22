634e043c7db7f81b05179f6e8501537ba08864d8
yeongbin-gong <youngbin816@gmail.com>
2023-02-22 12:43:09 +0900

    axios install

---
+++ src/TransactionAmountOfThePreviousDayMoreThan100BillionWon.svelte
@@ -0,0 +1,22 @@
+<script>
+  import axios from 'axios'
+	import { onMount } from 'svelte';
+
+  let transactionAmountOfThePreviousDayMoreThan100BillionWonData
+
+  onMount(async () => {
+    try {
+      // transactionAmountOfThePreviousDayMoreThan100BillionWonData = await axios.get('/st/transactionAmountOfThePreviousDayMoreThan100BillionWon')
+    } catch (error) {
+      console.log(error)
+    }
+  })
+</script>
+
+<h3>전일 거래대금 1000억 이상</h3>
+
+<ul>
+  {#each transactionAmountOfThePreviousDayMoreThan100BillionWonData as transactionAmountOfThePreviousDayMoreThan100BillionWon}
+      <li style="list-style: none;">{transactionAmountOfThePreviousDayMoreThan100BillionWon}</li>
+  {/each}
+</ul>
